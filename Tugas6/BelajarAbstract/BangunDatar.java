e7a3b642aa9cfaa80b43e5566d58b2c79c9430d5
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-11-18 21:22:55 +0700

    Create BangunDatar.java

---
+++ Tugas6/BelajarAbstract/BangunDatar.java
@@ -0,0 +1,13 @@
+public abstract class BangunDatar{
+    String warna;
+
+    String getWarna(){
+        return warna;
+    }
+    void setWarna(String warna){
+        this.warna = warna;
+    }
+    abstract float getLuas();
+    
+
+}

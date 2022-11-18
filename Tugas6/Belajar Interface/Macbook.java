1a5b3ec9c40c2cb266fb859bf55edaa9ca5cd367
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-11-18 21:35:34 +0700

    Create Macbook.java

---
+++ Tugas6/Belajar Interface/Macbook.java
@@ -0,0 +1,51 @@
+public class Macbook implements Laptop{
+    private int volume;
+    boolean is_power_on;
+    
+    public Macbook(){
+        this.volume = 50;
+    }
+    
+    @Override
+    public void powerOn() {
+        is_power_on = true;
+        System.out.println("Laptop is On");
+        System.out.println("Hello User MacBook Pro M2");
+    }
+
+    @Override
+    public void powerOff() {
+        is_power_on = false;
+        System.out.println("Shutdown is process ... ");
+    }
+
+    @Override
+    public void volumeUp() {
+        if(is_power_on){
+            if(this.volume == MAX_VOL){
+                System.out.println("Volume is Full");
+            }
+            else{
+                this.volume += 10;
+                System.out.println("Volume is : " + this.getVolume());
+            }
+        }
+    }
+
+    @Override
+    public void volumeDown() {
+        if(is_power_on){
+            if(this.volume == MIN_VOL){
+                System.out.println("Volume is 0%");
+            }
+            else{
+                this.volume -= 10;
+                System.out.println("Volume is : " + this.getVolume());
+            }
+        }
+    }
+    
+    public int getVolume(){
+        return this.volume;
+    }
+}

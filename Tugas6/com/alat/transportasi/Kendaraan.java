f6d4b4de225cd2100e40b39afb1889738bcf7599
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-11-18 21:41:17 +0700

    Create Kendaraan.java

---
+++ Tugas6/com/alat/transportasi/Kendaraan.java
@@ -0,0 +1,8 @@
+package com.alat.transportasi;
+import com.alat.transportasi.beroda.KendaraanBeroda;
+
+public class Kendaraan {
+    public static void main(String[] args){
+        System.out.println("Belajar Package dan interrface");
+    }
+}

f0bde08fb583ef66e65faba7d03d1b8566ee642a
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-11-18 21:25:53 +0700

    Create Circle.java

---
+++ Tugas6/BelajarAbstract/Circle.java
@@ -0,0 +1,14 @@
+public class Circle extends BangunDatar{
+
+    float jari_jari;
+    
+    public Circle(float jari_jari){
+        this.jari_jari = jari_jari;
+    }
+    
+    @Override
+    float getLuas() {
+        return (float) Math.PI*jari_jari*jari_jari;
+    }
+    
+}

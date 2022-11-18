5395bc5614b361f21f8cb400780236eacc2163a0
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-11-18 21:40:27 +0700

    Create KendaraanBeroda.java

---
+++ Tugas6/com/alat/transportasi/beroda/KendaraanBeroda.java
@@ -0,0 +1,7 @@
+package com.alat.transportasi.beroda;
+
+public class KendaraanBeroda {
+    public static void main(String[] args) {
+        System.out.println("Belajar Package dan Interface");
+    }
+}

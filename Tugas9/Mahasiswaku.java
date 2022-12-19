3a99064d6a4e687d4eec0a2ca65bebc9cdf4a466
VerdiEza <114009794+VerdiEza@users.noreply.github.com>
2022-12-19 22:39:30 +0700

    Create Mahasiswaku.java

---
+++ Tugas9/Mahasiswaku.java
@@ -0,0 +1,12 @@
+public class Mahasiswaku {
+    
+    public static void main(String[] args) {
+        Mahasiswa <String,String, Integer> m = new Mahasiswa<>();
+        m. setNim("1102020");
+        m. setName ("Ferdi");
+        m. setClas(21);
+        System.out.println(m.getNim());
+        System.out.println(m.getName ());
+        System.out.println(m.getClas());
+    }
+}
